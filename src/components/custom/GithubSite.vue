5b254fadead744fb59891f919debe36ada379271
久治明千树汐 <admin@soraharu.com>
2023-02-18 23:31:25 +0800

    First commit

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center text-neutral-500">
+    <span>❤️ Explore with</span>
+    <a href="https://soraharu.com/" target="_blank" class="text-blue-500">
+      XiaoXi
+    </a>
+  </div>
+</template>
